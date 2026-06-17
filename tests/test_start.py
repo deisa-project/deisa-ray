a1b0a22f3c5019b9cf64bcd75cce8f7f9a3fbca2
theabm <andres.bermeo-marinelli@inria.fr>
2026-06-17 12:48:55 +0200

    refactor

--- tests/test_start.py
+++ tests/test_start.py
@@ -23,7 +23,7 @@ DIST_TIMEOUT_ERRORS = tuple(
     if err is not None
 )
 
-@ray.remote
+@ray.remote(max_retries=0)
 def head_script() -> bool:
     """The head node checks that the values are correct"""
     from deisa.ray.window_handler import Deisa
@@ -37,8 +37,8 @@ def head_script() -> bool:
     d.execute_callbacks()
     return True
 
-@ray.remote
-def bridge_script(rank, port):
+@ray.remote(num_cpus=0, max_retries=0)
+def bridge_script(*, rank: int, port: int):
     from deisa.ray.bridge import Bridge
     from tests.comm_utils import init_gloo_comm
 
@@ -49,57 +49,52 @@ def bridge_script(rank, port):
             "chunk_position": (0, rank),
         }
     }
-    try:
-        comm = init_gloo_comm(
-            2,
-            rank,
-            "127.0.0.1",
-            port,
-            timeout_s=10,
-        )
-        b = Bridge(
-            arrays_metadata=arrays_md,
-            comm=comm,
-        )  # type:ignore
-    # TODO split exception into two cases: one for init_gloo_comm having some timeout, and another for Bridges
-    # not joining within threshold time. 
-    except Exception:
-        raise
-
-    return b.node_id, f"sched-{b.node_id}"
+    comm = init_gloo_comm(
+        2,
+        rank,
+        "127.0.0.1",
+        port,
+        timeout_s=1,
+    )
+    b = Bridge(
+        arrays_metadata=arrays_md,
+        comm=comm,
+    )  # type:ignore
 
 @pytest.mark.parametrize("sleep_t", [5])
 def test_sim_start_first_and_analytics_can_start_after_x_secs(ray_multinode_cluster, sleep_t):
     gloo_port = pick_free_port()
     cluster = ray_multinode_cluster["cluster"]
     head_node_id = None
-    worker_nodes = []
+    worker_node_ids = []
     for node in cluster.list_all_nodes():
         if node.is_head():
             head_node_id = node.node_id
         else:
-            worker_nodes.append(node.node_id)
+            worker_node_ids.append(node.node_id)
 
     # submit sim first
-    ref_sim = []
-    for i, w_nid in enumerate(worker_nodes):
-        ref_sim.append(
-            bridge_script.options(scheduling_strategy=NodeAffinitySchedulingStrategy(node_id=w_nid, soft=False)).remote(
-                i, gloo_port
-            )
-        )
+    worker_refs = [
+        bridge_script.options(
+            scheduling_strategy=NodeAffinitySchedulingStrategy(
+                node_id=worker_node_ids[rank],
+                soft=False,
+            ),
+        ).remote(rank=rank, port=gloo_port)
+        for rank in range(2)
+    ]
 
     # submit analytics after sleep_t seconds
     time.sleep(sleep_t)
-    ref_analytics = head_script.options(
-        max_retries=0,
-        scheduling_strategy=NodeAffinitySchedulingStrategy(node_id=head_node_id, soft=False),
+    head_ref = head_script.options(
+        scheduling_strategy=NodeAffinitySchedulingStrategy(
+            node_id=head_node_id,
+            soft=False,
+        )
     ).remote()
 
-    sim_res = ray.get(ref_sim)
-    for i, (n_id, _) in enumerate(sim_res):
-        assert n_id == worker_nodes[i]
-    assert ray.get(ref_analytics)
+    ray.get([head_ref] + worker_refs)
+
 
 
 @pytest.mark.parametrize("sleep_t", [5])
@@ -107,32 +102,35 @@ def test_analytics_start_first_and_sim_can_start_after_x_secs(ray_multinode_clus
     gloo_port = pick_free_port()
     cluster = ray_multinode_cluster["cluster"]
     head_node_id = None
-    worker_nodes = []
+    worker_node_ids = []
     for node in cluster.list_all_nodes():
         if node.is_head():
             head_node_id = node.node_id
         else:
-            worker_nodes.append(node.node_id)
+            worker_node_ids.append(node.node_id)
 
     # submit analytics first
-    ref_analytics = head_script.options(
-        max_retries=0,
-        scheduling_strategy=NodeAffinitySchedulingStrategy(node_id=head_node_id, soft=False),
+    head_ref = head_script.options(
+        scheduling_strategy=NodeAffinitySchedulingStrategy(
+            node_id=head_node_id,
+            soft=False,
+        ),
     ).remote()
     time.sleep(sleep_t)
-    # submit sim after sleep_t seconds
-    ref_sim = []
-    for i, w_nid in enumerate(worker_nodes):
-        ref_sim.append(
-            bridge_script.options(scheduling_strategy=NodeAffinitySchedulingStrategy(node_id=w_nid, soft=False)).remote(
-                i, gloo_port
-            )
-        )
 
-    sim_res = ray.get(ref_sim)
-    for i, (n_id, _) in enumerate(sim_res):
-        assert n_id == worker_nodes[i]
-    assert ray.get(ref_analytics)
+    # submit sim after sleep_t seconds
+    worker_refs = [
+        bridge_script.options(
+            scheduling_strategy=NodeAffinitySchedulingStrategy(
+                node_id=worker_node_ids[rank],
+                soft=False,
+            ),
+        ).remote(rank=rank, port=gloo_port)
+        for rank in range(2)
+    ]
+
+    ray.get([head_ref] + worker_refs)
+
 
 
 def test_sim_raise_if_not_enough_bridges_connect(ray_multinode_cluster):
@@ -140,27 +138,28 @@ def test_sim_raise_if_not_enough_bridges_connect(ray_multinode_cluster):
         gloo_port = pick_free_port()
         cluster = ray_multinode_cluster["cluster"]
         head_node_id = None
-        worker_nodes = []
+        worker_node_ids = []
         for node in cluster.list_all_nodes():
             if node.is_head():
                 head_node_id = node.node_id
             else:
-                worker_nodes.append(node.node_id)
+                worker_node_ids.append(node.node_id)
 
-        ref_analytics = head_script.options( 
-            max_retries=0,
+        head_ref = head_script.options(
             scheduling_strategy=NodeAffinitySchedulingStrategy(
-                node_id=head_node_id, soft=False),
+                node_id=head_node_id,
+                soft=False,
+            ),
         ).remote()
-        ref_sim = []
+
         # NOTE: we only have 1 node (so only 1 Bridges will be created)
-        w_nid = worker_nodes[0]
-        ref_sim.append(
-                bridge_script.options(scheduling_strategy=NodeAffinitySchedulingStrategy(node_id=w_nid, soft=False)).remote(
-                    0, gloo_port
-                )
-            )
-        sim_res = ray.get(ref_sim)
-        for i, (n_id, _) in enumerate(sim_res):
-            assert n_id == worker_nodes[i]
-        assert ray.get(ref_analytics)
+        worker_refs = [
+            bridge_script.options(
+                scheduling_strategy=NodeAffinitySchedulingStrategy(
+                    node_id=worker_node_ids[0],
+                    soft=False,
+                ),
+            ).remote(rank=0, port=gloo_port)
+        ]
+
+        ray.get([head_ref] + worker_refs)
